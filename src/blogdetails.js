cb654bbe7773f4cb60a4314853e1883d802bf674
isactes <fidel.renovato@gmail.com>
2022-08-08 19:32:22 -0500

    XC

--- src/blogdetails.js
+++ src/blogdetails.js
@@ -4,20 +4,20 @@ import useFetch from "./usefecth";
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blogs, isPending, error } = useFetch('http://localhost:3000/blogs/' + id);
-    console.log({blogs})
+    // console.log({});
     return ( 
         <div className="blog-details">
             { isPending && <div>Loading...</div> }
             { error && <div>error</div> }
-            { blogs && (
-                <article>
+            { blogs && (                <article>
                     <h2>{ blogs.title }</h2>
                     <p> Written by { blogs.author }</p>
                     <div>{ blogs.body }</div>
+                    
                 </article>    
             )}
         </div>
      );
 }
  
-export default BlogDetails;
+export default BlogDetails; 
